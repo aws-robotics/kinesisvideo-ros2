48555f0fd5475bfa3aa072e79e6b0f3e6e7ff899
Avishay Alon <avishaya@amazon.com>
2019-07-31 02:34:15 +0000

    Set log4cplus_config path override appropriately.

--- kinesis_video_streamer/launch/kinesis_video_streamer.launch.py
+++ kinesis_video_streamer/launch/kinesis_video_streamer.launch.py
@@ -15,25 +15,38 @@ import os
 from ament_index_python.packages import get_package_share_directory
 import launch
 import launch_ros.actions
-import re
+import yaml
+import shutil
 
 # Argument names
 NODE_NAME = "node_name"
 CONFIG = "config"
+MAX_STRING_LEN = 128
 
-def update_logger_config_path():
+def get_logger_config_path():
   logger_config_path = os.path.join(get_package_share_directory('kinesis_video_streamer'),
     'config', 'kvs_log_configuration')
   default_config = os.path.join(get_package_share_directory('kinesis_video_streamer'),
     'config', 'sample_config.yaml')
+
   with open(default_config, 'r') as f:
       config_text = f.read()
-  updated_config_text = re.sub('log4cplus_config:(.*)', 'log4cplus_config: ' + logger_config_path, config_text)
-  with open(default_config, 'w') as f:
-      f.write(updated_config_text)
+  config_yaml = yaml.safe_load(config_text)
+
+  fallback_path = '/tmp/kvs_log_configuration'
+  if fallback_path == config_yaml['kinesis_video_streamer']['ros__parameters']['kinesis_video']['log4cplus_config']:
+    if len(logger_config_path) <= MAX_STRING_LEN:
+      # Override /tmp/... to the actual path
+      return logger_config_path
+    else:
+      # Path too long and no alternative was specified - copy to /tmp/ fallback path (platform-dependent)
+      shutil.copy(logger_config_path, fallback_path)
+      return fallback_path
+  else:
+    # The config file was modified by the user - do not override
+    return None
 
 def generate_launch_description():
-  update_logger_config_path()
   # Default to included config file
   default_config = os.path.join(get_package_share_directory('kinesis_video_streamer'),
     'config', 'sample_config.yaml')
@@ -48,11 +61,17 @@ def generate_launch_description():
       default_value=default_config
     )
    ])
+
+  logger_config_path = get_logger_config_path()
+  if logger_config_path is None:
+    node_parameters = [launch.substitutions.LaunchConfiguration(CONFIG)]
+  else:
+    node_parameters = [launch.substitutions.LaunchConfiguration(CONFIG), {'log4cplus_config': logger_config_path}]
   streamer_node = launch_ros.actions.Node(
     package="kinesis_video_streamer",
     node_executable="kinesis_video_streamer",
     node_name=launch.substitutions.LaunchConfiguration(NODE_NAME),
-    parameters=[launch.substitutions.LaunchConfiguration(CONFIG)]
+    parameters=node_parameters
   )
 
   ld.add_action(streamer_node)
